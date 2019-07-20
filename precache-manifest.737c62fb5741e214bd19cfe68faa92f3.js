146b00c025e4561eca442b31c3ca1ddfe5fbc8df
kadirijhay <39244828+KadiriJhay@users.noreply.github.com>
2019-07-20 19:17:20 +0100

    Updates

---
+++ precache-manifest.737c62fb5741e214bd19cfe68faa92f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1e87bcbd545a39403d6686c065870e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "04c83323ebbb4db9ddc3",
+    "url": "./static/css/main.132bca5c.chunk.css"
+  },
+  {
+    "revision": "f0a5fafb0e2193df36c9",
+    "url": "./static/js/2.b684e7ec.chunk.js"
+  },
+  {
+    "revision": "04c83323ebbb4db9ddc3",
+    "url": "./static/js/main.ebbd34ad.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b3356bbede9f483b011e85835ee93be6",
+    "url": "./static/media/yaoming.b3356bbe.png"
+  }
+]);
